cdad7146b7fbda70e05115e15e805a03a9722086
Amauri Siqueira <amauri.ferreira.siqueira@gmail.com>
2025-03-18 15:11:49 -0300

    chapter 9.1

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -31,11 +31,13 @@ export async function fetchRevenue() {
 }
 
 export async function fetchLatestInvoices() {
+
+  // return 0;
   try {
 
     console.log('process.env.POSTGRES_URL');
-    console.log(process.env.POSTGRES_URL);
-    
+    console.log(process.env.POSTGRES_URL)!;
+
 
     const data = await sql<LatestInvoiceRaw[]>`SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
        FROM invoices
