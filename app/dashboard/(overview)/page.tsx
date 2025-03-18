727bf788af069d355f6e3801cca0b351c5694a25
Amauri Siqueira <amauri.ferreira.siqueira@gmail.com>
2025-03-18 14:29:23 -0300

    chapter 9

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -7,8 +7,10 @@ import {   fetchLatestInvoices , fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton } from '@/app/ui/skeletons';
 
+
 export default async function Page() {
 
+  
   
   const latestInvoices = await fetchLatestInvoices();
   
@@ -37,10 +39,10 @@ export default async function Page() {
         /> }
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-      <Suspense fallback={<RevenueChartSkeleton />}>
-         { <RevenueChart/> }
+         <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
         </Suspense>
-        { <LatestInvoices latestInvoices={latestInvoices} /> }
+         <LatestInvoices latestInvoices={latestInvoices} /> 
       </div>
     </main>
   );
